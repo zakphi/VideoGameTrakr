8ca680e3d568d7deb014594e4d84f09f2786ecc4
Philip Zak <zakphi@gmail.com>
2017-09-06 16:35:12 -0400

    create routes for games

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'platforms#index'
 
-  resources :platforms
+  resources :platforms do
+    resources :video_games
+  end
 end
