064511901bd72927628756e343340d48ab1aa788
Philip Zak <zakphi@gmail.com>
2017-09-10 16:21:27 -0400

    change the query for finding a game

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -1,7 +1,7 @@
 class VideoGamesController < ApplicationController
   def show
     @platform = Platform.find(params[:platform_id])
-    @video_game = VideoGame.where(id: params[:id])
+    @video_game = VideoGame.find(params[:id])
   end
 
   def destroy
